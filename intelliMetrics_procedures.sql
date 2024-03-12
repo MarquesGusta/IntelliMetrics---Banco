43da456c02bfc403b3f38fb283f0d4874c68b0a7
Gustavo Marques <91503740+MarquesGusta@users.noreply.github.com>
2024-03-12 20:02:27 -0300

    Add files via upload

---
+++ intelliMetrics_procedures.sql
@@ -0,0 +1,97 @@
+-- criação de usuário
+
+
+-- alteração de usuário
+
+
+-- exclusão de usuário
+
+
+-- redefinição de senha
+
+
+-- criação de cliente
+
+
+-- alteração de cliente
+
+
+-- exclusão de cliente
+
+
+-- mudança do representante
+
+
+-- criação de ordem de calibração
+
+
+-- alteração de ordem de calibração
+
+
+-- criação de categorias
+
+
+-- alteração de categorias
+
+
+-- exclusão de categorias
+
+
+-- criação de tipos
+
+
+-- alteração de tipos
+
+
+-- exclusão de tipos
+
+
+-- cadastro de instrumentos
+
+
+-- alteraçao de instrumentos
+
+
+-- exclusão de instrumentos / a decidir
+
+
+-- inserção de instrumentos recebidos
+
+
+-- alteração de instrumentos recebidos
+
+
+-- criação de certificados
+
+
+-- alteração de texto de um certificado
+
+
+-- alteração dos resultados de um certificado
+
+
+-- criação de ordem de medição
+
+
+-- alteração de ordem de medição
+
+
+-- cadastro de peças
+
+
+-- alteraçao de peças
+
+
+-- exclusão de peças / a decidir
+
+
+-- inserção de pecas recebidas
+
+
+-- alteração de pecas recebidas
+
+
+-- criação de relatório
+
+
+-- alteração de relatório
